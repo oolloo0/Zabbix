22cd2a46882c4c7ca7f280463db3bb654adb2c68
oolloo0 <126964955+oolloo0@users.noreply.github.com>
2025-02-14 23:36:12 +0100

    Update check_new_ad_user.ps1

--- AD_Monitoring_Zabbix/scripts/check_new_ad_user.ps1
+++ AD_Monitoring_Zabbix/scripts/check_new_ad_user.ps1
@@ -1,13 +1,2 @@
 $Events = Get-WinEvent -FilterHashtable @{LogName='Security'; ID=4720; StartTime=(Get-Date).AddMinutes(-5)} -ErrorAction SilentlyContinue
-if ($Events) {
-    foreach ($Event in $Events) {
-        $Details = @{
-            TimeCreated = $Event.TimeCreated
-            Creator = ($Event.Properties[1].Value)
-            NewUser = ($Event.Properties[0].Value)
-        }
-        Write-Output "Time: $($Details.TimeCreated), Created By: $($Details.Creator), New User: $($Details.NewUser)"
-    }
-} else {
-    Write-Output "No events found"
-}
+if ($Events) { Write-Host 1 } else { Write-Host 0 }
