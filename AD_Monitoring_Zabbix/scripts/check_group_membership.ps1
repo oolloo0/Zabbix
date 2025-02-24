13bb19afdcc66a8617d2ccb2b03bcfd10ca45a62
oolloo0 <126964955+oolloo0@users.noreply.github.com>
2025-02-24 19:19:22 +0100

    Update check_group_membership.ps1

--- AD_Monitoring_Zabbix/scripts/check_group_membership.ps1
+++ AD_Monitoring_Zabbix/scripts/check_group_membership.ps1
@@ -1,2 +1,31 @@
-$Events = Get-WinEvent -FilterHashtable @{LogName='Security'; ID=4728,4729; StartTime=(Get-Date).AddMinutes(-5)} -ErrorAction SilentlyContinue
-if ($Events) { Write-Host 1 } else { Write-Host 0 }
+$minutesToCheck = 5
+$startTime = (Get-Date).AddMinutes(-$minutesToCheck)
+
+$events = Get-WinEvent -FilterHashtable @{
+        LogName   = 'Security'
+        ID        = 4728, 4729
+        StartTime = $startTime
+    } -ErrorAction SilentlyContinue
+
+if ($events) {
+    foreach ($event in $events) {
+        $xml = [xml]$event.ToXml()
+        $eventData = @{}
+        $xml.Event.EventData.Data | ForEach-Object { 
+            $eventData[$_.Name] = $_.'#text' 
+        }
+        $member = ($eventData.MemberName -split ',')[0] -replace 'CN=',''
+
+        switch ($event.Id) {
+            4728 { 
+                Write-Output "Added $member to $($eventData.TargetUserName) by $($eventData.SubjectUserName)"
+            }
+            4729 { 
+                Write-Output "Removed $member from $($eventData.TargetUserName) by $($eventData.SubjectUserName)"
+            }
+        }
+    }
+}
+else {
+    Write-Output "No group membership changes"
+}
