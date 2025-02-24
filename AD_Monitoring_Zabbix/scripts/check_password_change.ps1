bee8fea91d7cd5e1aa58eb9682ac4166b36e4f3c
oolloo0 <126964955+oolloo0@users.noreply.github.com>
2025-02-24 19:20:24 +0100

    Update check_password_change.ps1

--- AD_Monitoring_Zabbix/scripts/check_password_change.ps1
+++ AD_Monitoring_Zabbix/scripts/check_password_change.ps1
@@ -1,2 +1,15 @@
-$Events = Get-WinEvent -FilterHashtable @{LogName='Security'; ID=4724; StartTime=(Get-Date).AddMinutes(-5)} -ErrorAction SilentlyContinue
-if ($Events) { Write-Host 1 } else { Write-Host 0 }
+$events = Get-WinEvent -FilterHashtable @{
+  LogName = 'Security'; ID = 4724;
+  StartTime = (Get-Date).AddMinutes(-5)
+}
+if ($events) {
+  foreach ($event in $events) {
+    $xml = [xml]$event.ToXml()
+    $whoDidIt  = ($xml.Event.EventData.Data | Where-Object { $_.Name -eq 'SubjectUserName' }).'#text'
+    $whoWasChanged = ($xml.Event.EventData.Data | Where-Object { $_.Name -eq 'TargetUserName' }).'#text'
+    Write-Output "Password changed by $whoDidIt for $whoWasChanged"
+  }
+} else {
+  Write-Output "No password change events"
+}
+
